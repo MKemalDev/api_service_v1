be594046508235bf62566336ca9a09888892ddee
kemal aksakal <mustafaksakal2525@gmail.com>
2024-02-02 17:57:52 +0300

    create connection.php

---
+++ app/DAL/Connection.php
@@ -0,0 +1,24 @@
+<?php
+require_once __DIR__ . "/../../vendor/autoload.php";
+
+use Doctrine\ORM\Tools\Setup;
+use Doctrine\ORM\EntityManager;
+
+$isDevMode = true;
+$config = Setup::createAnnotationMetadataConfiguration(
+    [__DIR__ . "/Model"],
+    $isDevMode,
+    null,
+    null,
+    false
+);
+
+$conn = [
+    'driver' => 'pdo_mysql',
+    'host' => 'localhost',
+    'user' => 'root',
+    'password' => '',
+    'dbname' => 'default_db',
+];
+
+$entityManager = EntityManager::create($conn, $config);

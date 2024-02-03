b7208f204df2bdd7213c9967fb059dfe9812cffa
kemal aksakal <mustafaksakal2525@gmail.com>
2024-02-03 10:27:11 +0300

    update product model

--- App/DAL/Models/ProductModel.php
+++ App/DAL/Models/ProductModel.php
@@ -21,6 +21,31 @@ class ProductModel extends BaseModel
      */
     protected $seo_slug_url;
 
+    /**
+     * @ORM\Column(type="string")
+     */
+    protected $description;
+
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    protected $sub_description;
+
+    /**
+     * @ORM\Column(type="float")
+     */
+    protected $price;
+
+    /**
+     * @ORM\Column(type="float")
+     */
+    protected $discount;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    protected $stock;
 
     /**
      * @ORM\ManyToOne(targetEntity="ProductTypeModel", inversedBy="products")

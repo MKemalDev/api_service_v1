5647dd72f1853fac5ceb778529d8e23a53a6d435
kemal aksakal <mustafaksakal2525@gmail.com>
2024-02-03 13:14:54 +0300

    create category model

---
+++ App/DAL/Models/CategoryModel.php
@@ -0,0 +1,44 @@
+<?php
+
+
+namespace App\DAL\Models;
+
+use Doctrine\ORM\Mapping as ORM;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="categories")
+ */
+
+class CategoryModel extends BaseModel
+{
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $seo_slug_url;
+
+    /**
+     * @ORM\OneToMany(targetEntity="SubCategoryModel", mappedBy="categoryModel")
+     */
+    private $subCategories;
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->subCategories = new ArrayCollection();
+    }
+
+    public function getSubCategories(): Collection
+    {
+        return $this->subCategories;
+    }
+
+}

70353db57f7bc0ff1f22f2af52a518b129b1f653
kemal aksakal <mustafaksakal2525@gmail.com>
2024-02-03 15:48:20 +0300

    update model model

--- App/DAL/Models/ModelModel.php
+++ App/DAL/Models/ModelModel.php
@@ -41,11 +41,40 @@ class ModelModel extends BaseModel
     {
         parent::__construct();
         $this->products = new ArrayCollection();
+        $this->images = new ArrayCollection();
     }
 
     public function getProducts(): Collection
     {
         return $this->products;
+    }
+
+
+    /**
+     * @ORM\ManyToMany(targetEntity="ImageModel", cascade={"persist"})
+     * @ORM\JoinTable(name="model_images",
+     *      joinColumns={@ORM\JoinColumn(name="model_id", referencedColumnName="id")},
+     *      inverseJoinColumns={@ORM\JoinColumn(name="image_id", referencedColumnName="id")}
+     * )
+     */
+    protected $images;
+
+
+    public function addImage(ImageModel $image)
+    {
+        if (!$this->images->contains($image)) {
+            $this->images[] = $image;
+        }
+    }
+
+    public function removeImage(ImageModel $image)
+    {
+        $this->images->removeElement($image);
+    }
+
+    public function getImages()
+    {
+        return $this->images;
     }
 
 }

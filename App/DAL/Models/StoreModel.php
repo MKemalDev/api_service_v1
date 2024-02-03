073f1f967aeb6961e82961c424d1f91afdd9afd4
kemal aksakal <mustafaksakal2525@gmail.com>
2024-02-03 15:59:46 +0300

    update store model

--- App/DAL/Models/StoreModel.php
+++ App/DAL/Models/StoreModel.php
@@ -40,12 +40,40 @@ class StoreModel extends BaseModel
     {
         parent::__construct();
         $this->products = new ArrayCollection();
+        $this->images = new ArrayCollection();
 
     }
 
     public function getProducts(): Collection
     {
         return $this->products;
+    }
+
+
+    /**
+     * @ORM\ManyToMany(targetEntity="ImageModel", cascade={"persist","remove"})
+     * @ORM\JoinTable(name="store_images",
+     *      joinColumns={@ORM\JoinColumn(name="store_id", referencedColumnName="id")},
+     *      inverseJoinColumns={@ORM\JoinColumn(name="image_id", referencedColumnName="id")}
+     * )
+     */
+    protected $images;
+
+    public function addImage(ImageModel $image)
+    {
+        if (!$this->images->contains($image)) {
+            $this->images[] = $image;
+        }
+    }
+
+    public function removeImage(ImageModel $image)
+    {
+        $this->images->removeElement($image);
+    }
+
+    public function getImages()
+    {
+        return $this->images;
     }
 
 }
